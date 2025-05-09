07ee7c0e3fc8dec09152bda3b9957bcf0907b6ee
julianesja <julianesja@gmail.com>
2025-05-09 15:45:36 -0500

    se actualizan las dependencias

--- AccountingTools.Model/Dtos/ExcelFileDto.cs
+++ AccountingTools.Model/Dtos/ExcelFileDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AccountingTools.Model.Dtos
+﻿namespace AccountingTools.Model.Dtos
 {
     public class ExcelFileDto<T>
     {
